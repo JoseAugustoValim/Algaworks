23773be9480d243debbddfb6eca6505b64507a20
JAV1994 <jose.valim@escola.pr.gov.br>
2025-04-09 14:46:03 -0300

    feat: commit a ser mergeado

--- 13 Polimorfismo/src/com/algaworks/banco/CaixaEletronico.java
+++ 13 Polimorfismo/src/com/algaworks/banco/CaixaEletronico.java
@@ -13,22 +13,21 @@ public class CaixaEletronico {
     }
 
     public void imprimirDemonstrativo(Conta conta) {
-        // conta é visto como um objeto de superclasse
-        // logo se eu preciso acessar os metodos da subclasse
-        // necessário fazer um downcast
-
-        // Essa e uma coercao explicita e pode levar a problemas
-        // caso o objeto nao seja de um subtipo direto da coercao
-        // Evite isto
-//        ContaInvestimento contaInvestimento = (ContaInvestimento) conta;
-
-        if (contaInvestimento.getValorTotalRendimentos() > 0) {
-            System.out.println("Impressão de Demonstrativo gratuita!!");
-        } else {
-            System.out.printf("Custo da impressão: R$%.2f%n", TARIFA_IMPRESSAO_DEMONSTRATIVO);
-            conta.sacar(TARIFA_IMPRESSAO_DEMONSTRATIVO);
+        if (conta instanceof ContaInvestimento) {
+            ContaInvestimento contaInvestimento = (ContaInvestimento) conta;
+            if (contaInvestimento.getValorTotalRendimentos() > 0) {
+                System.out.println("Impressão de Demonstrativo gratuita!!");
+            } else {
+                debitarTarifaImpressaoDemonstrativo(conta);
+            }
         }
 
+
         conta.imprimirDemonstrativo();
+    }
+
+    private static void debitarTarifaImpressaoDemonstrativo(Conta conta) {
+        System.out.printf("Custo da impressão: R$%.2f%n", TARIFA_IMPRESSAO_DEMONSTRATIVO);
+        conta.sacar(TARIFA_IMPRESSAO_DEMONSTRATIVO);
     }
 }
