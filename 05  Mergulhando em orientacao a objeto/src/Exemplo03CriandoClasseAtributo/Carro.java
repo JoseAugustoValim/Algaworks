ce33726a3097ef2f326d453901f78be2691c669e
JAV1994 <jose.valim@escola.pr.gov.br>
2025-04-24 11:54:36 -0300

    Modulo 5 MErgulando em Orientacao a Objeto

---
+++ 05  Mergulhando em orientacao a objeto/src/Exemplo03CriandoClasseAtributo/Carro.java
@@ -0,0 +1,12 @@
+package Exemplo03CriandoClasseAtributo;
+
+public class Carro {
+    // Membro da classe
+    // Variavel de instancia - variavel que irá existir quando houver uma instancia de carro
+    String fabricante;
+    String modelo;
+    String cor;
+    int anoFabricacao;
+
+    
+}
---
+++ 05  Mergulhando em orientacao a objeto/src/Main.java
@@ -0,0 +1,14 @@
+/*
+Objetos
+Representação da existência/instância/ocorrência que possui características unicas
+
+Classe
+Descrição do conjunto de características e comportamentos de um objeto
+
+
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
