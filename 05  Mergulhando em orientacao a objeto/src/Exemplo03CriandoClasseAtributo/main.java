5b5bd4939a0d602f1f82f8c5baad9a0f1c0b5447
José Augusto Valim <joseaugustovalim1994@gmail.com>
2025-04-28 14:39:39 -0300

    feat: exemplo carro

---
+++ 05  Mergulhando em orientacao a objeto/src/Exemplo03CriandoClasseAtributo/main.java
@@ -0,0 +1,13 @@
+package Exemplo03CriandoClasseAtributo;
+
+public class main {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.anoFabricacao = 2021;
+        carro.cor = "Azul";
+
+        Carro carro2 = new Carro();
+        carro2.anoFabricacao = 2022;
+        carro2.cor = "rosa";
+    }
+}
